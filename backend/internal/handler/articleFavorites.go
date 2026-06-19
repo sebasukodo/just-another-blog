fa8f6c4dee4751a3813654e2ddd97d8d0a8a714a
sebasukodo <231564377+sebasukodo@users.noreply.github.com>
2026-06-19 21:05:30 +0200

    adding unfavorite article handler

--- backend/internal/handler/articleFavorites.go
+++ backend/internal/handler/articleFavorites.go
@@ -55,4 +55,43 @@ func (h *Handler) FavoriteArticle(w http.ResponseWriter, r *http.Request) {
 
 func (h *Handler) UnfavoriteArticle(w http.ResponseWriter, r *http.Request) {
 
+	slug := r.PathValue("slug")
+
+	user, ok := r.Context().Value(contextKeyUser).(database.User)
+	if !ok {
+		h.RespondWithError(w, 401, "access denied", "missing user context")
+		return
+	}
+
+	article, err := h.DbQueries.GetArticleBySlug(r.Context(), slug)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			h.RespondWithError(w, 404, "not found", fmt.Sprintf("UnfavoriteArticle request failed, no article found for slug %v", slug))
+		} else {
+			h.RespondWithDatabaseError(w, err)
+		}
+		return
+	}
+
+	if err = h.DbQueries.UnfavoriteArticle(r.Context(), database.UnfavoriteArticleParams{
+		ArticleID: article.ID,
+		UserID:    user.ID,
+	}); err != nil {
+		h.RespondWithDatabaseError(w, err)
+		return
+	}
+
+	article.FavoriteCount -= 1
+
+	following, err := h.DbQueries.IsFollowing(r.Context(), database.IsFollowingParams{
+		FollowerID:  user.ID,
+		FollowingID: article.AuthorID,
+	})
+	if err != nil {
+		h.RespondWithDatabaseError(w, err)
+		return
+	}
+
+	h.RespondWithJSON(w, 200, buildArticleResponse(article, following, false))
+
 }
